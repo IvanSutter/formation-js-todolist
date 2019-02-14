a18756582d6b66796f682397e1e5578574b787e0
Ivan SUTTER <ivansutter@gmail.com>
2019-02-14 12:07:33 +0100

    Add Contact route

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { componentFactoryName } from '@angular/compiler';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
     path: '', component: HomeComponent
+  },
+  {
+    path: 'contact', component: ContactComponent
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, ContactComponent]
 })
 export class AppRoutingModule { }
